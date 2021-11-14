f32d4eac3c794720a41aab893f6f2bfe647bad9e
Demon <zero2211@protonmail.com>
2021-11-14 16:55:28 -0500

    workout models and server file finished

--- models/Workouts.js
+++ models/Workouts.js
@@ -2,35 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workouts = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  extercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
-    extercises: [
-        {
-            type: {
-                type: String
-            },
-            name: {
-                type: String
-            },
-            duration: {
-                type: Number
-            },
-            weight: {
-                type: Number
-            },
-            reps: {
-                type: Number
-            },
-            sets: {
-                type: Number
-            }
-        }
-    ]
+  ],
+  totalDur: {
+    type: Number,
+  },
+});
 
-})
+const Workout = mongoose.model("Workout", workouts);
 
-const Workout = mongoose.model("Workout", workouts)
-
-module.exports = Workout
+module.exports = Workout;
